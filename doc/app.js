a3da2ffa5a031712cab947a3b5ad1b3d789c110a
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2022-09-21 10:13:46 +1000

    start over

---
+++ doc/app.js
@@ -0,0 +1,7 @@
+function readMore(){
+    if($("#text-show-more").hasClass("hide-text")){
+        $("#text-show-more").removeClass("hide-text");
+    }else{
+        $("#text-show-more").addClass("hide-text");
+    }
+}
